abd1719817bda614e1f69d8df1c43deff9789277
LegoOow <castel.kevin.04@gmail.com>
2022-05-30 16:20:41 +0200

    Test signup & login

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -22,17 +22,27 @@ exports.getAllComment = (req, res, next) => {
 
 };
 
-// Create Post //
+// Create Comment //
 exports.createComment = (req, res, next) => {
-    const comment = {
-        userId: req.body.userId,
-        title: req.body.title
-    }; 
-    
-    Comment.create(comment)
-        .then(() => res.status(201).json({ message: "Commentaire ajouté !" }))
-        .catch(error => res.status(400).json({ error }));
-};
+    db.Post.findOne({
+        where: { id: req.params.postId }
+    })
+    .then(postFound => {
+        if(postFound) {
+            const comment = db.Comment.build({
+                content: req.body.content,
+                postId: postFound.id,
+                userId: userId
+            })
+            comment.save()
+                .then(() => res.status(201).json({ message: 'Votre commentaire a bien été créé !' }))
+                .catch(error => res.status(400).json({ error: '⚠ Oops, une erreur s\'est produite !' }));
+        } else {
+            return res.status(404).json({ error: 'Message non trouvé'})
+        }
+    })
+    .catch(error => res.status(500).json({ error: '⚠ Oops, une erreur s\'est produite !' }));
+}
 
 // Delete Comment //
 
